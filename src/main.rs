df3d778b2106ed934391c3e2e62b4ad5dadd4319
coderrrrr <saadwaqar211@gmail.com>
2025-07-04 21:19:36 +0500

    tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[allow(unused_imports)]
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    for stream in listener.incoming(){
+        match stream {
+            Ok(_stream) => {
+                println!("New connection accepted")
+            }
+            Err(e) => {
+                println!("Error: {}", e)
+            }
+        }
+    }
 }
